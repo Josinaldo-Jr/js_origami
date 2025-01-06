9085b2189f062ff4fdd776ec96af01d70e1c457d
Josinaldo-Jr <josinaldoeuzebiojr@gmail.com>
2025-01-06 12:37:39 -0300

    feat: add smooth scroll for internal links

--- 3-Elementos/scroll_suave_link_interno.js
+++ 3-Elementos/scroll_suave_link_interno.js
@@ -30,17 +30,19 @@ function initAccordion() {
   const accordionList = document.querySelectorAll(".js-accordion dt");
   const activeClass = "ativo";
 
-  accordionList[0].classList.add(activeClass);
-  accordionList[0].nextElementSibling.classList.add(activeClass);
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
   }
-
-  accordionList.forEach((item) => {
-    item.addEventListener("click", activeAccordion);
-  });
 }
 
 initAccordion();
@@ -50,20 +52,50 @@ initAccordion();
 function initScrollSuave() {
   const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
 
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
+  if (linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
     });
   }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
 }
 
 initScrollSuave();
+
+/*------------------Animação ao Scroll------------------*/
+
+function initAnimaScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowHalf = window.innerHeight * 0.6;
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionVisible = sectionTop - windowHalf < 0;
+
+        if (sectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimaScroll();
