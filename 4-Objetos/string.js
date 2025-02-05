c7d39cd95100e99383a65842f1654803a343cf83
Josinaldo-Jr <josinaldoeuzebiojr@gmail.com>
2025-02-05 16:24:41 -0300

    feat: finish String practice

---
+++ 4-Objetos/string.js
@@ -0,0 +1,84 @@
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes = [
+    {
+      descricao: 'Taxa do Pão',
+      valor: 'R$ 39',
+    },
+    {
+      descricao: 'Taxa do Mercado',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 99',
+    },
+    {
+      descricao: 'Taxa do Banco',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 49',
+    },
+  ];
+
+let totalTaxa = 0;
+let totalRecebimento = 0;
+
+transacoes.forEach(transacao => {
+  const valorNumerico = parseFloat(transacao.valor.replace("R$", ""));
+
+  if (transacao.descricao.includes("Taxa")) {
+    totalTaxa += valorNumerico;
+  } else {
+    totalRecebimento += valorNumerico;
+  }  
+});
+
+console.log(`Total de Taxas: ${totalTaxa}.`)
+console.log(`Total de Recebimentos: ${totalRecebimento}.`)
+
+  
+  // Retorne uma array com a lista abaixo
+  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  const arrayItens = transportes.split(";");
+
+  console.log(arrayItens);
+
+
+  
+  // Substitua todos os span's por a's
+ let html = ` <ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+              </ul>`;
+html = html.replace(/<span>/g, '<a>').replace(/<\/span>/g, '</a>');
+console.log(html);             
+  
+  // Retorne o último caracter da frase
+  const frase = 'Melhor do ano!';
+  /*const ultimoCaracter = frase.slice(13, 14);*/
+  /*const ultimoCaracter = frase.slice(-1);*/
+  const ultimoCaracter = frase.charAt(frase.length-1);
+
+  console.log(ultimoCaracter);
+  
+  // Retorne o total de taxas
+  const transacoes_2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+  // let totalTaxa_2 = 0;
+  // const textoTaxa = transacoes_2.forEach(transacao => {
+  //   let resultado = transacao.toLowerCase().includes("taxa");
+  //  if (resultado === true) {
+  //   totalTaxa_2++;
+  //  }
+
+  // });
+
+  const totalTaxas = transacoes_2.filter(transacao => 
+      transacao.toLowerCase().includes('taxa')
+  ).length;
+  
+  console.log(totalTaxas);  
