a0cf66d07ca10c1b2f5faaba65f927b2d2e9dd03
Josinaldo-Jr <josinaldoeuzebiojr@gmail.com>
2025-04-09 09:44:23 -0300

    feat: add modal lesson

---
+++ 5-Efeitos-no-DOM/modal/script.js
@@ -0,0 +1,37 @@
+// --------------------------------
+// Modulos:
+
+import initTabNav from "./modules/initTabNav.js";
+import initAccordion from "./modules/initAccordion.js";
+import initScrollSuave from "./modules/initScrollSuave.js";
+import initAnimaScroll from "./modules/initAnimaScroll.js";
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimaScroll();
+
+// --------------------------------
+
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+const sectionsShowDown = document.querySelectorAll('[data-anime="show-down"]');
+const sectionsShowRight = document.querySelectorAll('[data-anime="show-right"]');
+
+sectionsShowDown.forEach(section => section.classList.add("show-down"));
+sectionsShowRight.forEach(section => section.classList.add("show-right"));
+
+
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+// Substitua todas as classes js- por data atributes.
+
+// --------------------------------
